2ff5dd143820d4be5b0a0d60b7c4dd2a7c4a04f7
hlo <herbet.hadrien@gmail.com>
2023-06-02 10:32:42 +0200

    [ADD] adding unittest and some fix on a few function

---
+++ tests/functional_test/booking_place.py
@@ -0,0 +1,36 @@
+import unittest
+from flask import Flask, session
+from server import app, loadClubs, loadCompetitions, loadPlaces
+
+class FlaskTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.data_comps = loadCompetitions()
+        self.data_clubs = loadClubs()
+        self.data_places = loadPlaces()
+
+    def tearDown(self):
+        pass
+
+    def test_Success_purchase(self):
+        client = self.app.test_client()
+        
+        form_data = {
+            'competition': 'Tibo Inshape Tournament',
+            'club': 'Simply Lift',
+            'places': '3'
+        }
+        with client.session_transaction() as session:
+            session['user_p11'] = "john@simplylift.co"
+
+        response = client.post('/purchasePlaces', data=form_data)
+        self.assertEqual(response.status_code, 200)
+        
+        expected_page_name = "Resume"
+        rendered_template = response.get_data(as_text=True)
+        self.assertIn(expected_page_name, rendered_template)
+
+if __name__ == '__main__':
+    unittest.main()
